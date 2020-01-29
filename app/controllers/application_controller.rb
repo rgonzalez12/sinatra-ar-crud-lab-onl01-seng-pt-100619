72ee1a87ef90d615fd9ed068fb63ce34989bfa5b
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-29 06:33:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,12 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
+  patch 'articles/:id' do
+    article = Article.find(params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+    redirect to "/articles"
+  
   
   
   
